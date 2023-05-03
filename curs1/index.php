88142a2bb27810e04805592a4d9b4386e5c8d9f8
Ecaterina <barganecaterina@yahoo.com>
2023-05-03 21:05:52 +0300

    test knowledges in if/if else - low

--- curs1/index.php
+++ curs1/index.php
@@ -1,15 +1,5 @@
 <?php
 
-$age = 10;
-$y = 20;
 
-$z = "Text";
-$_test = "Test";
-
-$b = TRUE;
-
-define("NAME","Thomas"); //trecand true ca ultima valoare, numele variabilei nu este sensibil la majuscule si minuscule
-
-echo NAME;
 
 ?>
