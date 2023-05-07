972b020ed20e83de6b8a15fe3360f9468b72c93e
Ecaterina <barganecaterina@yahoo.com>
2023-05-07 21:57:14 +0300

    function

--- curs1/index.php
+++ curs1/index.php
@@ -1,3 +1,21 @@
 <?php
+//m-am oprit la v 10 din 19
+
+
+
+function calculate($a = 10, $b = 10) {
+  global $x;
+  $x = 10;
+  $c = $a + $b + $x;
+  return $c;
+}
+
+
+
+echo calculate();
+echo $x;
+// definind variabila x ca fiind global in interiorul functiei o facem accesibila si inafara ei, insa doar dupa ce este apelata pentru ca functia nu reactioneaza la intalnirea parserului/a blocului de cod ci la apelare.
+
+
 
 ?>
