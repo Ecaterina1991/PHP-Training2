bdb73b352d14f53bd1022de77aebc5130ab883a5
Ecaterina <barganecaterina@yahoo.com>
2023-05-07 22:56:23 +0300

    array

--- curs1/index.php
+++ curs1/index.php
@@ -1,21 +1,3 @@
 <?php
-//m-am oprit la v 10 din 19
-
-
-
-function calculate($a = 10, $b = 10) {
-  global $x;
-  $x = 10;
-  $c = $a + $b + $x;
-  return $c;
-}
-
-
-
-echo calculate();
-echo $x;
-// definind variabila x ca fiind global in interiorul functiei o facem accesibila si inafara ei, insa doar dupa ce este apelata pentru ca functia nu reactioneaza la intalnirea parserului/a blocului de cod ci la apelare.
-
-
-
+//m-am oprit la 12 video
 ?>
