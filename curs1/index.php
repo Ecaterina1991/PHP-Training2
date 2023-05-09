ac1c256348ca962677ead0cfb0c3cdc2686f79ed
Ecaterina <barganecaterina@yahoo.com>
2023-05-09 22:19:25 +0300

    whitelist

--- curs1/index.php
+++ curs1/index.php
@@ -1,43 +1,13 @@
 <?php
+$whitelist = array("home", "page1", "page2", "page3");
 
-$status = "admin2";
+if(isset($_GET['page'])) {
 
-$numbers = array(
-              "012 256 365",
-              "012 256 562",
-              "012 256 412",
-              "012 256 965",
-              "012 256 785"
-                );
-
-if($status === "admin") {
-  foreach($numbers as $index => $value) {
-    echo "Number " . $index . ": " . $value . "<br>";
-   } 
+  if(in_array($_GET['page'], $whitelist)) {
+    include($_GET['page'] . ".php");
   } else {
-  // foreach($numbers as $index => $value) {
-  //   $value = substr_replace($value, "*** *", 4, 5);
-  //   echo "Number " . $index . ": " . $value . "<br>";
-//}    
-
-foreach($numbers as $index => $value) {
-    $num = "";
-  
-    for($i=0; $i < strlen($value); $i++) {
-
-     if($i>3 && $i < (strlen($value) - 2) && $i!=7) {
-       $num .= "*";
-     }
-
-     else {
-      $num .= $value[$i];
-     }
-
-    }
-
-    echo "Number " . $index . ": " . $num . "<br>";
- }
-
+    include("home.php");
+  }
 }
 
 ?>
