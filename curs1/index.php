03e4952a859a8fd679edc592a7e4a891c90215da
Ecaterina <barganecaterina@yahoo.com>
2023-05-09 21:46:50 +0300

    work with array2

--- curs1/index.php
+++ curs1/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$status = "admin";
+$status = "admin2";
 
 $numbers = array(
               "012 256 365",
@@ -10,15 +10,34 @@ $numbers = array(
               "012 256 785"
                 );
 
-if($status === "admin2") {
+if($status === "admin") {
   foreach($numbers as $index => $value) {
     echo "Number " . $index . ": " . $value . "<br>";
    } 
   } else {
-  foreach($numbers as $index => $value) {
-    $value = substr_replace($value, "*** *", 4, 5);
-    echo "Number " . $index . ": " . $value . "<br>";
-  }    
+  // foreach($numbers as $index => $value) {
+  //   $value = substr_replace($value, "*** *", 4, 5);
+  //   echo "Number " . $index . ": " . $value . "<br>";
+//}    
+
+foreach($numbers as $index => $value) {
+    $num = "";
+  
+    for($i=0; $i < strlen($value); $i++) {
+
+     if($i>3 && $i < (strlen($value) - 2) && $i!=7) {
+       $num .= "*";
+     }
+
+     else {
+      $num .= $value[$i];
+     }
+
+    }
+
+    echo "Number " . $index . ": " . $num . "<br>";
+ }
+
 }
 
 ?>
