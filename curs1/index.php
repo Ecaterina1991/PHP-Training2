ebfe67e2f99adc38948e4375b5b2204313474b38
Ecaterina <barganecaterina@yahoo.com>
2023-05-12 21:18:57 +0300

    good mysql connection

--- curs1/index.php
+++ curs1/index.php
@@ -1,31 +1,13 @@
 <?php
-// Parametrii de conexiune la baza de date
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "test";
-
-// Crearea conexiunii
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificarea conexiunii
-if ($conn->connect_error) {
-    die("Conexiunea a esuat: " . $conn->connect_error);
+$dbhost = '127.0.0.1';
+$dbuser = 'root';
+$dbpass = '';
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass);
+if($mysqli -> connect_errno) {
+  printf("connection failed", $mysqli-> connect_error);
+  exit();
 }
 
-// Interogare pentru selectarea datelor din tabel
-$sql = "SELECT * FROM tabelul_meu";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Parcurgerea și afișarea rezultatelor
-    while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Nume: " . $row["nume"] . "<br>";
-    }
-} else {
-    echo "Nu s-au găsit înregistrări în tabel.";
-}
-
-// Închiderea conexiunii
-$conn->close();
+printf("connected successfully");
+$mysqli->close();
 ?>
--- curs1/page1.php
+++ curs1/page1.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "Page1";
+$lista_studenti = array(1=>'Alexandra', 2=>'Cecilia', 3=>'Madalina', 4=>'Cristian', 5=>'Sorina');
+
+$index = 0;
+
+
+while($index <= sizeof($lista_studenti)) {
+echo $lista_studenti[$index] . "<br>";
+$index++;
+}
 
 ?>
