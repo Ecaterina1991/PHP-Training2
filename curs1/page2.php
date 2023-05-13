0edf8ff80f9d2a1694b517ecfc431a24b37fe7fe
Ecaterina <barganecaterina@yahoo.com>
2023-05-13 21:13:00 +0300

    curs1:

--- curs1/page2.php
+++ curs1/page2.php
@@ -1,5 +1,11 @@
+
 <?php
+$arr = array("Alexandra"=>"cursant avansat phpacademy", "Sorin"=>"cursant avansat phpacademy", "Hora"=>"cursant intermediar phpacademy", "Ecaterina"=>"cursant intermediar phpacademy", "Nicoleta"=>"cursant incepator phpacademy");
 
-echo "Page2";
+array_walk($arr, 'arrFunc', ' este un cursant cu inalte aptitudini antrenat intr-un curs intensiv si profesionist de programare si in cadrul capitolului Basic PHP detine statutul de ');
 
-?>
+function arrFunc($value, $key, $p) {
+	echo $key.$p.$value."<br>"."<br>";
+} 
+
+ ?>
