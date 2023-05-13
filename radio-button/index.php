f3cd352c7bceb8345b4b60d6c167a711147fd9a9
Ecaterina <barganecaterina@yahoo.com>
2023-05-13 23:54:16 +0300

    radio-button/index:

--- radio-button/index.php
+++ radio-button/index.php
@@ -10,15 +10,39 @@
   <form action="index.php" method="post">
    <input type="radio" name="credit_card" value="Visa">
    Visa<br>
-   <input type="radio" name="credit-card" value="Mastercard">
+   <input type="radio" name="credit_card" value="Mastercard">
    Mastercard<br>
-   <input type="radio" name="credit-card" value="American Express">
+   <input type="radio" name="credit_card" value="American Express">
   American Express<br>
-  <input type="submit" name="confirm" value="confirm>
+  <input type="submit" name="confirm" value="confirm">
   </form>
 </body>
 </html>
 
 <?php
-$credit_card = 
+
+if(isset($_POST["confirm"])) {
+
+  $credit_card = null;
+  //i-am atribuit null ca sa nu ne apara warning atunci cand nu bifam nimic
+   
+  if (isset($_POST["credit_card"])) {
+      $credit_card = $_POST["credit_card"];
+  } 
+
+  if($credit_card == "Visa") {
+    echo "You selected Visa";
+  }
+  elseif($credit_card == "Mastercard") {
+    echo "You selected Mastercard";
+  }
+  elseif($credit_card == "American Express") {
+    echo "You selected American Express";
+  }
+  else {
+    echo "Please make a selection";
+  }
+}
+
+
 ?>
