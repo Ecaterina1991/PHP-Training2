42f8b8fc882c74f24816f30f2e6e41d76755946b
Ecaterina <barganecaterina@yahoo.com>
2023-05-13 22:38:07 +0300

    small form

--- curs1/page2.php
+++ curs1/page2.php
@@ -1,11 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<form action="page2.php" method="post">
+<label for="country">Enter a country</label>
+<input type="text" name="country">
+<input type="submit">
+</form>
+  
+</body>
+</html>
+
 
 <?php
-$arr = array("Alexandra"=>"cursant avansat phpacademy", "Sorin"=>"cursant avansat phpacademy", "Hora"=>"cursant intermediar phpacademy", "Ecaterina"=>"cursant intermediar phpacademy", "Nicoleta"=>"cursant incepator phpacademy");
 
-array_walk($arr, 'arrFunc', ' este un cursant cu inalte aptitudini antrenat intr-un curs intensiv si profesionist de programare si in cadrul capitolului Basic PHP detine statutul de ');
+$capitals = array("USA"=>"Washington DC",
+                 "Japan"=>"Kyoto",
+                 "South Korea" => "Seoul",
+                 "India"=>"New Delhi");
 
-function arrFunc($value, $key, $p) {
-	echo $key.$p.$value."<br>"."<br>";
-} 
+                 $capital = $capitals[$_POST["country"]];
+                 echo "The capital is {$capital}";
 
  ?>
