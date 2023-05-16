14e01cee927f8a8869974ce5a967bc3c2ac39c80
Ecaterina <barganecaterina@yahoo.com>
2023-05-16 22:46:21 +0300

    php-mate:

---
+++ php-mate/index.php
@@ -0,0 +1,75 @@
+<?php
+// Exercițiu simplu:
+// Scrieți o buclă care afișează toate numerele de la 1 la 10 și pentru fiecare număr, afișați valoarea sa absolută utilizând funcția abs().
+
+
+// for($i=1; $i<=10; $i++) {
+//   echo abs($i) . "<br>";
+// }
+
+// Exercițiu mediu:
+//Scrieți o buclă care afișează primele 20 de numere întregi și pentru fiecare număr, afișați rădăcina sa pătrată utilizând funcția sqrt().
+
+// for($i=1; $i<=20; $i++) {
+//   echo round(sqrt($i), 2) . "<br>";
+// }
+
+// Exercițiu mediu:
+// Generați 10 numere aleatoare între -50 și 50 utilizând funcția rand(). Afișați aceste numere și pentru fiecare număr, determinați dacă este pozitiv, negativ sau zero utilizând o structură de control (if-else).
+
+//1. generez 10 numere aleatorii intre -50 si 50
+
+// for($i=1; $i<=10; $i++) {
+//   $result = rand(-50, 50);  
+//   echo $result;
+
+// if($result > 0) {
+//   echo " este un numar pozitiv"."<br>";
+// }
+// else if ($result < 0) {
+//   echo " este un numar negativ"."<br>";
+// }
+// else {
+//   echo " numarul este egal cu zero"."<br>";
+// }
+// }
+
+//multumesc
+
+
+// Exercițiu mediu:
+// Scrieți o buclă care afișează toate numerele pare de la 1 la 50 și pentru fiecare număr, afișați rezultatul funcției round() aplicată asupra valorii respective.
+
+// for ($i=2; $i<=50; $i+=2) {
+//   echo round($i)."<br>";
+// }
+
+// Exercițiu mediu:
+// Generați 5 numere aleatoare între 1 și 100 utilizând funcția rand(). Afișați aceste numere și determinați suma lor utilizând o buclă și o variabilă auxiliară.
+
+
+// $sum=0;
+// for ($i=1; $i<=5; $i++) {
+// $nr_random = rand(1, 100);
+// echo $nr_random."<br>";
+// $sum = $sum + $nr_random;
+// }
+// echo $sum;
+
+//multumesc
+
+
+// ???Exercițiu mediu:
+// Scrieți o buclă care afișează primele 10 numere Fibonacci și pentru fiecare număr, afișați partea întreagă a rezultatului funcției sqrt() aplicată asupra valorii respective.
+
+
+// ???Exercițiu mediu:
+// Generați un număr aleatoriu între 1 și 100 utilizând funcția rand(). Apoi, scrieți o buclă care să determine cel mai mic multiplu comun (CMMD) dintre acel număr generat și toate numerele de la 1 la 10 utilizând funcția min().
+
+
+// ???Exercițiu mediu:
+// Scrieți o buclă care afișează toate numerele de la 1 la 100 și pentru fiecare număr, verificați dacă acesta este prim utilizând o structură de control (if-else) și funcția sqrt().
+
+
+
+?>
